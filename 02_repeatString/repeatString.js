1001b3d095a1d2d0f61c03a0bed0f3739781a67f
bonz99 <xbonz666@hotmail.com>
2022-06-23 21:20:12 +0300

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (let i = 0; i < repeatCount; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
