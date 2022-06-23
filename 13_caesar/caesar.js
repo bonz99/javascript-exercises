ae467a90e43ed081b1aafceb0dcdd3791bec5b55
bonz99 <xbonz666@hotmail.com>
2022-06-24 01:50:31 +0300

    Finish exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,30 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+    let newStr = "";
+    let charCode;
+    shift = shift % 26;
+    for (let char of str) {
+        charCode = char.charCodeAt(0);
+        if (charCode >= 97 && charCode <= 122) {
+            char = String.fromCharCode(charCode + shift);
+            charCode = char.charCodeAt(0);
+            if (charCode > 122) {
+                char = String.fromCharCode(charCode - 26);
+            } else if (charCode < 97) {
+                char = String.fromCharCode(charCode + 26);
+            }
+        }
+        else if (charCode >= 65 && charCode <= 90) {
+            char = String.fromCharCode(charCode + shift);
+            charCode = char.charCodeAt(0);
+            if (charCode > 90) {
+                char = String.fromCharCode(charCode - 26);
+            } else if (charCode < 65) {
+                char = String.fromCharCode(charCode + 26);
+            }
+        }
+        newStr += char;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
