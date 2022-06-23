830c5bce33876fbda98b837424b60498aebb2493
bonz99 <xbonz666@hotmail.com>
2022-06-24 00:45:07 +0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for (let person of people) {
+        if (person.yearOfDeath === undefined || person.yearOfDeath === null) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
