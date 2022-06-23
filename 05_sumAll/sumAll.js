6d8b9b9452169c3d05aa15e6440160b4750e2169
bonz99 <xbonz666@hotmail.com>
2022-06-23 22:26:17 +0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    if ((typeof(firstNumber) !== "number") || (typeof(lastNumber) !== "number") ||
+             (firstNumber < 0) || (lastNumber < 0)) {
+        return "ERROR";
+    }
 
+    if (firstNumber > lastNumber) {
+        [firstNumber, lastNumber] = [lastNumber, firstNumber];
+    }
+
+    let sum = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
