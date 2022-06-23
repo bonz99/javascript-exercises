3223fe310cabee9f084859788a2ed7332f0e0b26
bonz99 <xbonz666@hotmail.com>
2022-06-23 23:06:53 +0300

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const sum = function(numbers) {
+  let sumOfArray = 0;
+	for (let i = 0; i < numbers.length; i++) {
+    sumOfArray += numbers[i]; 
+  }
+  return sumOfArray;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  for (let number of numbers) {
+    product *= number;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let facOfNumber = 1;
+  for (let i = number; i > 0; i--) {
+    facOfNumber *= i;
+  }
+  return facOfNumber;
 };
 
 // Do not edit below this line
