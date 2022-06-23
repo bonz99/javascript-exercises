5652461aa2926064f3a51f9caa472580a8a17f6b
bonz99 <xbonz666@hotmail.com>
2022-06-23 21:59:35 +0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (let i = 0; i < array.length; i++) {
+        if (itemsToRemove.includes(array[i])) {
+            array.splice(array.indexOf(array[i]), 1);
+            i--;
+        } else {
+            continue;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
