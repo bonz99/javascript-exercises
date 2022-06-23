c988a56bbaeafba27002f203edfcf43b0664bae4
bonz99 <xbonz666@hotmail.com>
2022-06-23 23:33:35 +0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let strWithOnlyLetters = string.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let reversedString = strWithOnlyLetters.split("").reverse().join("");
+    return strWithOnlyLetters === reversedString;
 };
 
 // Do not edit below this line
