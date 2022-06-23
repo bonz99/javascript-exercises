6f75dd976e1fcc9c59da6dc8d8f9ec9d9ac02968
bonz99 <xbonz666@hotmail.com>
2022-06-24 00:04:54 +0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) { return "OOPS"}
+    let array = [1];
+    let num1 = 0;
+    let num2 = 1;
+    let sum = 0;
+    for (let i = 0; i < number-1; i++) {
+        sum = num1 + num2;
+        array.push(sum);
+        num1 = num2;
+        num2 = sum;
+    }
+    return array[array.length-1];
 };
 
 // Do not edit below this line
